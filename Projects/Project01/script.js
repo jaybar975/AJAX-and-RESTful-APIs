032d5c09ab3d5164ef3141a0b490a9f87955c612
Justin Aybar <jaybar975@west-mec.org>
2019-09-17 15:27:16 -0700

    Project01 Commit #1
    Started the project. Added my first name last name and today's date to the script.js and stocks.html page. Fixed something in the docker compose file because docker-compose up wasn't working.

--- Projects/Project01/script.js
+++ Projects/Project01/script.js
@@ -1,9 +1,13 @@
 /*  Project 01_11_02
 
-    Author: 
-    Date:   
+    Author: Justin Aybar  
+    Date: 9/17/2019
 
     Filename: script.js
 */
 
 "use strict";
+
+// global variables
+var httpRequest = false;
+var entry = "^IXIC";
